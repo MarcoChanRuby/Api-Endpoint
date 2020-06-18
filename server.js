4a851e66dfdc961a6c871bb68f622ae4c57a2a5c
Marco.TH.Chan <marco.th.cham@pccw.com>
2020-06-18 16:14:46 +0800

    txnType

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/txnHistory', function(request, response) {
 
         if (accId) {
           result = paymentData.filter(x => x.accId === parseInt(accId));
-          if(txnType){
+          if(txnType && txnType !=="A"){
             result = result.filter(x => x.txnType === txnType);
           }
         } else {
