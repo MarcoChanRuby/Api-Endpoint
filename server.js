4f402d73baee0634a2fe204b55b29b1ea214a42d
Marco.TH.Chan <marco.th.cham@pccw.com>
2020-06-18 12:28:46 +0800

    f

--- server.js
+++ server.js
@@ -16,30 +16,34 @@ app.get('/balanceHistory', function(request, response) {
             balance: 900000,
             history: [{txnId:1, txnType: "T", date: "2020-06-16", venue: "ABC Supermarket", currency: "HKD", amt: 10000}, {txnId:2, txnType: "U", date: "2020-06-17", venue: "BBC Store", currency: "HKD", amt: 9898}],
         };
-    
-    
-
-    // fs.readFile('./data/paymentHistory.json', 'utf8', (err, data) => {
-    //     if (err) {
-    //         throw err;
-    //     }
-         
-    //     var result;
-    //     var paymentData = JSON.parse(data);
-
-    //     if (accId) {
-    //       result = paymentData.filter(x => x.accId === accId);
-    //     } else {
-    //       result = paymentData;
-    //     }
-
-    //     response.setHeader('Access-Control-Allow-Origin','*');
-    //     response.send(JSON.parse(JSON.stringify(result)));
-    // });
 
     response.setHeader('Access-Control-Allow-Origin','*');
     response.send(JSON.parse(JSON.stringify(jsonContent)));
 });
+
+app.get('/balanceHistory/:accId', function(request, response) {
+    var accId = request.query.accId;
+    var txnType = request.query.txnType;
+    
+    fs.readFile('./data/paymentHistory.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+         
+        var result;
+        var paymentData = JSON.parse(data);
+
+        if (accId) {
+          result = paymentData.filter(x => x.accId === accId);
+        } else {
+          result = paymentData;
+        }
+
+        response.setHeader('Access-Control-Allow-Origin','*');
+        response.send(JSON.parse(JSON.stringify(result)));
+    });
+});
+
 
 
 app.listen(app.get('port'), function () {
